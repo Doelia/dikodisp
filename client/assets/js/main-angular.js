45d81fb6242de53f29db95208a9373ca2f11481d
Doelia <kyxalia@gmail.com>
2015-12-22 18:57:56 +0100

    Affichage de la liste des mot associés

--- client/assets/js/main-angular.js
+++ client/assets/js/main-angular.js
@@ -4,12 +4,13 @@ var app = angular.module('diko', []);
 app.controller('WordController', function($http, $scope) {
     var that = this;
 
-    that.word = {};
+    that.word = {}; // Structure Mot, contenant un array rel[] (liste des mots en relation), Def, et Name
     that.isLoad = false;
-    that.listTypes = [];
+    that.listTypes = []; // Liste des types des relations, unique
 
     var name = "chat"; // TODO
 
+    // Contruit la liste unique des types
     var buildTypes = function() {
         var rel = that.word.ListRel;
         var list = [];
@@ -19,32 +20,35 @@ app.controller('WordController', function($http, $scope) {
         list = $.unique(list);
 
         that.listTypes = [];
-        for (var i in list) {
+        for (i in list) {
             var r = {
                 label: list[i],
                 name: getFullNameOfType(list[i])
             };
             that.listTypes.push(r);
         }
-    }
+    };
 
-    var getWordsFromType = function(typeNeedle) {
-        var rel = that.listTypes;
+    // typeNeedle string label dlf r_associated
+    // Retourne un tableau de Ref (voir structures)
+    that.getWordsFromType = function(typeNeedle) {
+        console.log("typeNeedle = "+typeNeedle);
+        var rel = that.word.ListRel;
         var list = [];
         for (var i in rel) {
-            var r = rel[i];
-            if (r.Type == typeNeedle) {
-                list.push(r);
+            var labelType = rel[i].Type;
+            if (labelType == typeNeedle) {
+                list.push(rel[i]);
             }
         }
         return list;
-    }
+    };
 
     $http.get("/getWord?word="+name)
         .then(function(response) {
             that.word = response.data;
             buildTypes();
-            console.log(response);
+            // console.log(response);
             that.isLoad = true;
         });
 });
